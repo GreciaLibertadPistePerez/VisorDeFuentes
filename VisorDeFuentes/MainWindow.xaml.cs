e9eab88e1fbe3713cb990dc544c912cf823ff188
Grecia Libertad Pisté Pérez <19470356@itcampeche.edu.mx>
2020-10-27 13:53:25 -0600

    VisorDeFuentes: Agrega el manejo de eventos

--- VisorDeFuentes/MainWindow.xaml.cs
+++ VisorDeFuentes/MainWindow.xaml.cs
@@ -20,49 +20,66 @@ namespace VisorDeFuentes
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int FontSize_small = 22;
+        private const int FontSize_medium = 18;
+        private const int FontSize_large = 24;
         public MainWindow()
         {
+
             InitializeComponent();
         }
 
         private void fuenteComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            if (fuenteComboBox.SelectedItem != null)
+            {
+                string fuente = (string)(fuenteComboBox.SelectedItem as ComboBoxItem).Content;
+                leyendaTexBlock.FontFamily = new FontFamily(familyName: fuente);
+            }
         }
 
         private void cursivasCheckBox_Checked(object sender, RoutedEventArgs e)
         {
-
+            leyendaTexBlock.FontStyle = FontStyles.Italic;
         }
 
         private void cursivasCheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
-
+            leyendaTexBlock.FontStyle = FontStyles.Normal;
         }
 
         private void negritasCheckBox_Checked(object sender, RoutedEventArgs e)
         {
-
+            leyendaTexBlock.FontWeight = FontWeights.Bold;
         }
 
         private void negritasCheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
-
+            leyendaTexBlock.FontWeight = FontWeights.Normal;
         }
 
         private void smallRadioButton_Checked(object sender, RoutedEventArgs e)
         {
-
+            if (smallRadioButton.IsChecked == true)
+            {
+                leyendaTexBlock.FontSize = 12;
+            }
         }
 
         private void mediumRadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-
+        { 
+            if (mediumRadioButton.IsChecked == true) 
+            { 
+                leyendaTexBlock.FontSize = FontSize_medium; 
+            }
         }
-
+        
         private void largeRadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-
+        { 
+            
+            
+               leyendaTexBlock.FontSize = FontSize_large; 
+            
         }
     }
 }
